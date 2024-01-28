71a8529623f9f14ca2783111d92c976bf7c22583
techno-celebes <official@techno-celebes.com>
2024-01-28 21:03:40 +0800

    newest 280124:0903pm

---
+++ src/Providers/MasterViewProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MyVendor\MyPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class MasterViewProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/views/master.blade.php' => resource_path('views/master/master.blade.php'),
+        ], 'my-package-views');
+    }
+}
